c3fe6b5fdedb1f468cdb17310ff53b5c63ce154a
ИСП-22 <-22@LAB30-10>
2023-11-30 10:25:22 +0300

    Добавьте файлы проекта.

---
+++ OperatorFor5Praktos/Program.cs
@@ -0,0 +1,11 @@
+﻿
+var anltFunc = Math.Pow(Math.PI, 4) / 90;
+var sumFunc = 0.0;
+
+for (int n = 1; n <= 12; n++)
+{
+    sumFunc += 1 / Math.Pow(n, 4);
+};
+Console.WriteLine($"Расчетная функция\t=\t{Math.Round(sumFunc, 3)}\n" +
+                  $"Аналитическая функция\t=\t{Math.Round(anltFunc, 3)}");
+Console.ReadKey();
